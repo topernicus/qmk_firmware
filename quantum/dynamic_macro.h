71544e9028350c729d1b702f7727b86ca154634d
Colin T.A. Gray <colinta@gmail.com>
2017-11-21 20:59:52 -0700

    use enum values instead of magic ints for 'macro_state' (and rename from 'macro_id')

--- quantum/dynamic_macro.h
+++ quantum/dynamic_macro.h
@@ -177,6 +177,12 @@ void dynamic_macro_record_end(
     *macro_end = macro_pointer;
 }
 
+enum {
+  READY = 0,
+  RECORD1,
+  RECORD2
+};
+
 /* Handle the key events related to the dynamic macros. Should be
  * called from process_record_user() like this:
  *
@@ -233,19 +239,19 @@ bool process_record_dynamic_macro(uint16_t keycode, keyrecord_t *record)
 
     /* 0   - no macro is being recorded right now
      * 1,2 - either macro 1 or 2 is being recorded */
-    static uint8_t macro_id = 0;
+    static uint8_t macro_state = READY;
 
-    if (macro_id == 0) {
+    if (macro_state == READY) {
         /* No macro recording in progress. */
         if (!record->event.pressed) {
             switch (keycode) {
             case DYN_REC_START1:
                 dynamic_macro_record_start(&macro_pointer, macro_buffer);
-                macro_id = 1;
+                macro_state = RECORD1;
                 return false;
             case DYN_REC_START2:
                 dynamic_macro_record_start(&macro_pointer, r_macro_buffer);
-                macro_id = 2;
+                macro_state = RECORD2;
                 return false;
             case DYN_MACRO_PLAY1:
                 dynamic_macro_play(macro_buffer, macro_end, +1);
@@ -263,15 +269,15 @@ bool process_record_dynamic_macro(uint16_t keycode, keyrecord_t *record)
             if (record->event.pressed) { /* Ignore the initial release
                                           * just after the recoding
                                           * starts. */
-                switch (macro_id) {
-                case 1:
+                switch (macro_state) {
+                case RECORD1:
                     dynamic_macro_record_end(macro_buffer, macro_pointer, +1, &macro_end);
                     break;
-                case 2:
+                case RECORD2:
                     dynamic_macro_record_end(r_macro_buffer, macro_pointer, -1, &r_macro_end);
                     break;
                 }
-                macro_id = 0;
+                macro_state = READY;
             }
             return false;
         case DYN_MACRO_PLAY1:
@@ -280,11 +286,11 @@ bool process_record_dynamic_macro(uint16_t keycode, keyrecord_t *record)
             return false;
         default:
             /* Store the key in the macro buffer and process it normally. */
-            switch (macro_id) {
-            case 1:
+            switch (macro_state) {
+            case RECORD1:
                 dynamic_macro_record_key(macro_buffer, &macro_pointer, r_macro_end, +1, record);
                 break;
-            case 2:
+            case RECORD2:
                 dynamic_macro_record_key(r_macro_buffer, &macro_pointer, macro_end, -1, record);
                 break;
             }
